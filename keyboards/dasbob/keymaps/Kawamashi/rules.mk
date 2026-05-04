e627806593c97a52b2135ddb8ac8199144135a0e
Kawamashi <kawamashi@free.fr>
2026-05-04 16:56:27 +0200

    kbd rules.mk user_folder finder

--- keyboards/dasbob/keymaps/Kawamashi/rules.mk
+++ keyboards/dasbob/keymaps/Kawamashi/rules.mk
@@ -1,2 +1,11 @@
-ROOT_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
-include ${ROOT_DIR}../../../../users/Kawamashi/rules.mk
+USERS_DIR := $(shell \
+    dir="$(dir $(realpath $(lastword $(MAKEFILE_LIST))))"; \
+    while [ "$$dir" != "/" ]; do \
+        if [ -d "$$dir/users" ]; then \
+            echo "$$dir/users"; \
+            exit 0; \
+        fi; \
+        dir=$$(dirname "$$dir"); \
+    done \
+)
+include $(USERS_DIR)/Kawamashi/rules.mk
