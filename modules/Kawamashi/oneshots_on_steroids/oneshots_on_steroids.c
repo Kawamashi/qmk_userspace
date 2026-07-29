0f55435fce90efc96f60e6c9ce3da6c2478a7dde
Kawamashi <kawamashi@free.fr>
2026-07-29 10:15:45 +0200

    OS_STEROIDS_FREE_LAYER_STACK default layer
    OS_STEROIDS_FREE_LAYER_STACK can’t deactivate the default layer

--- modules/Kawamashi/oneshots_on_steroids/oneshots_on_steroids.c
+++ modules/Kawamashi/oneshots_on_steroids/oneshots_on_steroids.c
@@ -290,8 +290,8 @@ bool process_record_oneshots_on_steroids(uint16_t keycode, keyrecord_t *record){
                         // OSL on steroids can deactivate another layer only if there is no ongoing oneshot layer,
                         // not to mess up with the layer stack.
                             const uint8_t key_layer = read_source_layers_cache(record->event.key);
-                            //const uint8_t default_layer = get_highest_layer(default_layer_state);
-                            if (SHOULD_FREE_LAYER_STACK && key_layer > oneshot[i].layer) {// && key_layer != default_layer) {
+                            const uint8_t default_layer = get_highest_layer(default_layer_state);
+                            if (SHOULD_FREE_LAYER_STACK && key_layer > oneshot[i].layer && key_layer != default_layer) {
                                 oneshot_origin_layer = key_layer;
                                 layer_off(key_layer);
                             }
